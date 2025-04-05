9ef5736bd9208a3b7ef7287c708f4f75a20bc920
samilturksoy <samiltuksoy@gmail.com>
2025-04-05 20:09:12 +0300

    Implement QuizScreen with question navigation and scoring. Added state management for current question index and score, and styled components for better UI presentation.

---
+++ data/queations.ts
@@ -0,0 +1,59 @@
+const questions = {
+    history: [
+        {
+            question: "Osmanlı İmparatorluğu hangi yıl yıkıldı?",
+            options: ["1920", "1922", "1918", "1923"],
+            answerIndex: 1,
+            hint: "Cumhuriyetin ilanından bir yıl önce."
+        },
+        {
+            question: "Fransız Devrimi hangi yılda gerçekleşti?",
+            options: ["1789", "1776", "1804", "1812"],
+            answerIndex: 0,
+            hint: "18. yüzyılın sonlarında."
+        }
+    ],
+    general: [
+        {
+            question: "Dünyanın en büyük okyanusu hangisidir?",
+            options: ["Atlantik", "Hint", "Arktik", "Pasifik"],
+            answerIndex: 3,
+            hint: "En geniş yüzölçümüne sahip okyanus."
+        },
+        {
+            question: "Güneş sisteminde en büyük gezegen hangisidir?",
+            options: ["Mars", "Dünya", "Jüpiter", "Satürn"],
+            answerIndex: 2,
+            hint: "Büyük kırmızı lekeye sahip gezegen."
+        }
+    ],
+    music: [
+        {
+            question: "Beethoven hangi müzik dönemine aittir?",
+            options: ["Barok", "Klasik", "Romantik", "Modern"],
+            answerIndex: 2,
+            hint: "Mozart'tan sonra gelir."
+        },
+        {
+            question: "Michael Jackson hangi lakapla anılır?",
+            options: ["Rock’un Kralı", "Pop’un Kralı", "Soul’un Babası", "Jazz Ustası"],
+            answerIndex: 1,
+            hint: "Pop müziğin kralı olarak bilinir."
+        }
+    ],
+    movies: [
+        {
+            question: "Titanic filminin yönetmeni kimdir?",
+            options: ["Steven Spielberg", "James Cameron", "Christopher Nolan", "Peter Jackson"],
+            answerIndex: 1,
+            hint: "Avatar filminin yönetmeniyle aynı."
+        },
+        {
+            question: "Yüzüklerin Efendisi film serisi hangi yazara dayanır?",
+            options: ["J.K. Rowling", "J.R.R. Tolkien", "George R.R. Martin", "Stephen King"],
+            answerIndex: 1,
+            hint: "Orta Dünya'nın yaratıcısı."
+        }
+    ]
+};
+ export default questions;
--- screens/QuizScreen.tsx
+++ screens/QuizScreen.tsx
@@ -1,12 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import questions from '../data/queations';
 
 const QuizScreen = () => {
+  const route = useRoute();
+  const navigation =useNavigation();
+  const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
+  const [score, setscore] = useState(0);
+
+  // TODO: Add types
+  // @ts-ignore
+  const { category } = route.params;
+  //@ts-ignore
+  const quiezQuestions = questions[category];
+
+  const { question, answerIndex, options, hint } = quiezQuestions[currentQuestionIndex];
+
+  const handleAnswer = (option, optionIndex) => {
+
+    if (optionIndex === answerIndex) {
+      setscore((prev) => prev + 1);
+    }
+
+    // Tüm sorular yanıtlanmadıysa
+    if (currentQuestionIndex + 1 < quiezQuestions.length) {
+      setcurrentQuestionIndex((prev) => prev + 1);
+    }else{
+      // skor/total
+      // @ts-ignore
+      navigation.navigate("Result", {score,total:quiezQuestions.length});
+    }
+  }
   return (
-    <View>
-      <Text>QuizScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.question}>{question}</Text>
+      <View style={styles.optionContainer}>
+        {options.map((x, i) => (
+          
+          <TouchableOpacity key={x} style={styles.option} onPress={() => handleAnswer(x, i)}>
+            <Text adjustsFontSizeToFit numberOfLines={1} style={styles.optionText}>{x}</Text>
+          </TouchableOpacity>
+
+        ))}
+      </View>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    gap: 32
+  },
+  question: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  optionContainer: {
+    gap: 9,
+  },
+
+  option: {
+    padding: 8,
+    paddingHorizontal:32,
+    backgroundColor: "dodgerblue",
+
+  },
+  optionText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 24
+
+  }
+
+})
 export default QuizScreen
