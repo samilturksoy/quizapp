889be537f223bc386a703b6e84776a33ca06a022
samilturksoy <samiltuksoy@gmail.com>
2025-04-06 15:51:43 +0300

    Integrate gesture handling and bottom sheet functionality in QuizScreen. Updated App component to wrap StackNavigator with GestureHandlerRootView. Added new dependencies for gesture handling and bottom sheet support.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import StackNavigator from './navigation/StackNavigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  return <StackNavigator />;
+  
+  return (
+    <GestureHandlerRootView>
+      <StackNavigator />
+    </GestureHandlerRootView>
+  );
 }
--- screens/QuizScreen.tsx
+++ screens/QuizScreen.tsx
@@ -1,13 +1,18 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import questions from '../data/queations';
+import { BottomSheetView } from '@gorhom/bottom-sheet';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 const QuizScreen = () => {
   const route = useRoute();
-  const navigation =useNavigation();
+  const navigation = useNavigation();
   const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
   const [score, setscore] = useState(0);
+  const [showHint, setshowHint] = useState(false)
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
 
   // TODO: Add types
   // @ts-ignore
@@ -26,24 +31,41 @@ const QuizScreen = () => {
     // Tüm sorular yanıtlanmadıysa
     if (currentQuestionIndex + 1 < quiezQuestions.length) {
       setcurrentQuestionIndex((prev) => prev + 1);
-    }else{
+    } else {
       // skor/total
       // @ts-ignore
-      navigation.navigate("Result", {score,total:quiezQuestions.length});
+      navigation.navigate("Result", { score, total: quiezQuestions.length });
     }
   }
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    if (index === -1) {
+      setshowHint(false);
+    }
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.question}>{question}</Text>
       <View style={styles.optionContainer}>
         {options.map((x, i) => (
-          
+
           <TouchableOpacity key={x} style={styles.option} onPress={() => handleAnswer(x, i)}>
             <Text adjustsFontSizeToFit numberOfLines={1} style={styles.optionText}>{x}</Text>
           </TouchableOpacity>
 
         ))}
       </View>
+      <TouchableOpacity style={styles.hint} onPress={() => setshowHint(prev => !prev)}>
+        <Text style={styles.hintText}>i</Text>
+      </TouchableOpacity>
+      {
+        showHint && (<BottomSheet onChange={handleSheetChanges} ref={bottomSheetRef} enablePanDownToClose snapPoints={[200, "50%"]}>
+          <BottomSheetView style={styles.contentContainer}>
+            <Text style={styles.bottomSheetText}>İpucu 🎉</Text>
+            <Text style={styles.bottomSheetDescription}>{hint}</Text>
+          </BottomSheetView>
+        </BottomSheet>
+        )}
     </View>
   )
 }
@@ -63,10 +85,25 @@ const styles = StyleSheet.create({
   optionContainer: {
     gap: 9,
   },
-
+  hint: {
+    backgroundColor: "tomato",
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    position: "absolute",
+    bottom: 32,
+    right: 16,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  hintText: {
+    color: "white",
+    fontSize: 36,
+    fontWeight: "bold"
+  },
   option: {
     padding: 8,
-    paddingHorizontal:32,
+    paddingHorizontal: 32,
     backgroundColor: "dodgerblue",
 
   },
@@ -75,6 +112,18 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 24
 
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: 'center',
+    gap: 8
+  },
+  bottomSheetText: {
+    fontSize: 24
+  },
+  bottomSheetDescription: {
+    fontSize: 16
   }
 
 })
